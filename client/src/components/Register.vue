34d905bfa4836ff85a8addd3056df952b57dc979
beardedpayton <beardedpayton@gmail.com>
2018-02-12 10:35:03 -0500

    built out register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" v-model="email" name="email">
+    <br>
+    <input type="password" v-model="password" name="password">
+    <br>
+    <button @click="registerUser">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    registerUser () {
+      AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send(`The email returned is ${req.body.email}`)
+})
+
+app.listen(process.env.PORT || 8081)
